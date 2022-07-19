29c73746a3fad4940354d68ae04f7831d58fd357
Nikita Skorodumov <mr-zerx@yandex.ru>
2022-07-20 01:03:41 +0500

    init project

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import honey from './img/123.JPG';
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img src={honey} width='30%' height='30%'/>
+    </>
+);
+
+export default Module;
